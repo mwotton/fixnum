5c4937feb671577657c7d0112701fb29f4b77deb
Mark Wotton <mwotton@loki.(none)>
2010-04-13 15:16:33 +1000

    tighten up test bounds

--- spec/my_test_extn_spec.rb
+++ spec/my_test_extn_spec.rb
@@ -14,8 +14,12 @@ describe "my_test" do
     @m.add(1).should == 2
   end
   
-  it 'should add something' do
-    @m.add(100000000000).should == 100000000001
+  it 'should add 1 to something 32 bits long' do
+    @m.add(2147483647).should == 2147483648
+  end
+  
+  it 'should add 1 to something a bit bigger than 32 bits' do
+    @m.add(2147483648).should == 2147483649
   end
   
 end
